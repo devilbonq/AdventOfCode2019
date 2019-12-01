8206133cca3e42c4dc40dbf332780a2322987a55
Jakub Boncler <bonq661@gmail.com>
2019-12-02 00:48:30 +0100

    Day 1 - Part 2

--- Day1/day1.py
+++ Day1/day1.py
@@ -1,21 +1,42 @@
 from time import time
-import math
+from math import floor
 
 
-def read_file(inputFile):
-    with open(inputFile, "r") as f:
+def read_file(input_file):
+    with open(input_file, "r") as f:
         lines = f.readlines()
     return lines
+
+
+def get_fuel_req(mass):
+    return floor(int(mass)/3) - 2
 
 
 def function_a():
     start = time()
     fuel_req = 0
-    for x in read_file("input.txt"):
-        fuel_req += math.floor(int(x)/3) - 2
+    for mass in read_file("input.txt"):
+        fuel_req += get_fuel_req(mass)
+    return fuel_req, time() - start
+
+
+def function_b():
+    start = time()
+    fuel_req = 0
+
+    for mass in read_file("input.txt"):
+        module_mass_fuel_req = get_fuel_req(mass)
+        additional_fuel_cost = get_fuel_req(module_mass_fuel_req)
+        while additional_fuel_cost > 0:
+            prev_mass = module_mass_fuel_req
+            module_mass_fuel_req = module_mass_fuel_req + additional_fuel_cost
+            additional_fuel_cost = get_fuel_req(module_mass_fuel_req - prev_mass)
+        fuel_req += module_mass_fuel_req
     return fuel_req, time() - start
 
 
 if __name__ == '__main__':
     resultA, timeA = function_a()
     print("Fuel requirement: {}. Completed in {}.".format(resultA, timeA))
+    resultB, timeB = function_b()
+    print("Fuel requirement: {}. Completed in {}.".format(resultB, timeB))
