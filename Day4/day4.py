2356f1d85bba685a4a12b474302a92185b0cc5f8
Jakub Boncler <bonq661@gmail.com>
2019-12-15 10:31:26 +0100

    Day 4 - Part 2

--- Day4/day4.py
+++ Day4/day4.py
@@ -11,6 +11,24 @@ def get_range(line):
     return int(line.split("-")[0]), int(line.split("-")[1])
 
 
+def validate_number_not_descending(password):
+    password_meet_criteria = True
+    for iterator in range(0, 5):
+        if int(str(password)[iterator]) > int(str(password)[iterator + 1]):
+            password_meet_criteria = False
+            break
+    return password_meet_criteria
+
+
+def validate_digit_repeat(password):
+    repeated_twice = False
+    for element in list(set(password)):
+        if password.count(element) == 2:
+            repeated_twice = True
+            break
+    return repeated_twice
+
+
 def function_a():
     start = time()
     range_min, range_max = get_range(read_file("input.txt")[0])
@@ -21,16 +39,22 @@ def function_a():
             if str(password)[iterator] == str(password)[iterator+1]:
                 password_meet_criteria = True
         if password_meet_criteria:
-            for iterator in range(0, 5):
-                if password_meet_criteria and int(str(password)[iterator]) > int(str(password)[iterator+1]):
-                    password_meet_criteria = False
-                    break
-        if password_meet_criteria:
-            passwords_counter += 1
+            passwords_counter += validate_number_not_descending(password)
+    return passwords_counter, time() - start
 
+
+def function_b():
+    start = time()
+    passwords_counter = 0
+    range_min, range_max = get_range(read_file("input.txt")[0])
+    for password in range(range_min, range_max):
+        if validate_digit_repeat(str(password)):
+            passwords_counter += validate_number_not_descending(password)
     return passwords_counter, time() - start
 
 
 if __name__ == '__main__':
     resultA, timeA = function_a()
     print("Passwords that meet criteria: {}. Completed in {}.".format(resultA, timeA))
+    resultB, timeB = function_b()
+    print("Passwords that meet criteria: {}. Completed in {}.".format(resultB, timeB))
